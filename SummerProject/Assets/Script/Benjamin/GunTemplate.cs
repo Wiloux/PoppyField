5cae8aa68d7167e593133d94d70ad902863ca943
Benjamin Chariou <chariou.benjamin@gmail.com>
2020-06-24 12:15:53 +0200

    Tir + Créations de gun personalisables
    Clic droite, Create, Gun pour créer un nouveau scriptable object.

---
+++ SummerProject/Assets/Script/Benjamin/GunTemplate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Gun", menuName = "Gun")]
+public class GunTemplate : ScriptableObject
+{
+    public string gunName;
+
+    public int gunDamage;
+    public int nbAmmo;
+    public int maxAmmo;
+
+    public float reloadTime;
+
+    public AudioClip shootSound;
+
+    public Sprite bulletSprite;
+
+}
