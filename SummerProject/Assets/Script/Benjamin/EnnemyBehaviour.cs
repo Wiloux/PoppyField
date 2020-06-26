b5ff43f92f49c60c13804f61479d58283aa7f8c0
Benjamin Chariou <chariou.benjamin@gmail.com>
2020-06-27 00:00:11 +0200

    Ennemi

--- SummerProject/Assets/Script/Benjamin/EnnemyBehaviour.cs
+++ SummerProject/Assets/Script/Benjamin/EnnemyBehaviour.cs
@@ -1,11 +1,34 @@
 ﻿
 using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
 
 public class EnnemyBehaviour : MonoBehaviour
 {
     public float health = 50f;
+    public bool canMove = false;
+    public float speed;
+    public NavMeshAgent agent;
+    public Transform target;
+    public float stunTime;
+
+    private void Start()
+    {
+        agent.speed = speed;
+    }
+    private void Update()
+    {
+        if (canMove) { 
+        agent.SetDestination(target.position);
+        }
+    }
+
     public void TakeDamage(float amount)
     {
+        if (canMove)
+        {
+            StartCoroutine(Stun(stunTime));
+        }
         health -= amount;
         if (health <= 0f)
         {
@@ -15,5 +38,12 @@ public class EnnemyBehaviour : MonoBehaviour
     void Die()
     {
         Destroy(gameObject);
+    }
+
+    private IEnumerator Stun(float stunTime)
+    {
+        agent.speed = 0;
+        yield return new WaitForSeconds(stunTime);
+        agent.speed = speed;
     }
 }
