0e7c1c627f92a93c5900fddf7c719dd4be368af1
Comete99 <62999111+Comete99@users.noreply.github.com>
2020-06-26 15:45:05 +0200

    Debut système sauvegarde

---
+++ SummerProject/Assets/Script/Comète/Save_system.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Save_system : MonoBehaviour
+{
+    private int sceneIndex;
+    private int sceneToLoad;
+
+    public void SaveScene()
+    {
+        sceneIndex = SceneManager.GetActiveScene().buildIndex;
+        PlayerPrefs.SetInt("SavedScene", sceneIndex);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadGame()
+    {
+
+    }
+}
