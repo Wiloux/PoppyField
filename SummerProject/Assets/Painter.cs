421fac41ad437fb062d8b05153d49cd4227f58d1
Wiloux <54811449+Wiloux@users.noreply.github.com>
2020-06-27 04:57:55 +0200

    bcp de choses

---
+++ SummerProject/Assets/Painter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Painter : MonoBehaviour
+{
+    public Material Mat;
+    public float Hardness;
+    public Color Color;
+    public float w;
+    public Renderer Rend;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Mat.SetColor("Color_E0B335C9", Color);
+        Mat.SetVector("Vector3_3828B19D", (Vector4)this.transform.position);
+        Mat.SetFloat("Vector1_9C4FE784", this.transform.localScale.x * 0.5f);
+        Mat.SetFloat("Vector1_7B9012FA", Hardness);
+
+
+    
+    }
+
+    void IDK()
+    {
+        CommandBuffer command = new CommandBuffer();
+        command.name = "UV Space Renderer";
+        command.SetRenderTarget("");
+        command.DrawRenderer(Rend, Mat, 0);
+        Graphics.ExecuteCommandBuffer(command);
+    }
+}
