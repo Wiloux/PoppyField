0e7886443d8a846e680c7044541c71be9100fb88
Comete99 <62999111+Comete99@users.noreply.github.com>
2020-07-10 14:26:38 +0200

    Ajout données save system

--- SummerProject/Assets/Script/Comète/Test_saveSystem.cs
+++ SummerProject/Assets/Script/Comète/Test_saveSystem.cs
@@ -1,4 +1,5 @@
 ﻿using Invector.vCharacterController;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,19 +10,30 @@ public class Test_saveSystem : MonoBehaviour
     private GameObject Player;
     private GameObject Cam;
     public GameObject InGameUI;
-    public GameObject panelSave;
-    public GameObject panelLoad;
+    public GameObject panelSaveLoad;
+    public List<GameObject> saveLocations;
+    //public GameObject panelLoad;
 
     // Update is called once per frame
     private void Start()
     {
         Player = GameObject.FindGameObjectWithTag("Player");
         Cam = GameObject.FindGameObjectWithTag("MainCamera");
+        
+
     }
     private void OnTriggerStay(Collider collision)
     {
         if (Input.GetKeyDown("f") && collision.gameObject.tag == "Player")
         {
+            foreach (GameObject save in saveLocations)
+            {
+                GameObject text = save.transform.Find("Text").gameObject;
+                Text text1 = text.GetComponent<Text>();
+                var ts = TimeSpan.FromSeconds(PlayerPrefs.GetFloat("TotalPlaytime" + save.name));
+                string playTime = string.Format("{0:00}:{1:00}:{2:00}",ts.Hours, ts.Minutes, ts.Seconds);
+                text1.text = "Temps de jeu : " + playTime + "\nDernière Sauvegarde : " + PlayerPrefs.GetString("DateLastSave" + save.name) + "\nNombre de sauvegardes : " + PlayerPrefs.GetInt("NbSave" + save.name);
+            }
             ActivateSaveUI();
         }
     }
@@ -39,9 +51,9 @@ public class Test_saveSystem : MonoBehaviour
 
     public void ActivateSaveUI()
     {
-        panelLoad.SetActive(!panelLoad.activeSelf);
+        panelSaveLoad.SetActive(!panelSaveLoad.activeSelf);
         InGameUI.SetActive(!InGameUI.activeSelf);
-        if (panelLoad.activeSelf)
+        if (panelSaveLoad.activeSelf)
         {
             Time.timeScale = 0;
             Player.GetComponent<vThirdPersonInput>().enabled = false;
@@ -60,23 +72,23 @@ public class Test_saveSystem : MonoBehaviour
         }
     }
 
-    void LoadSystem()
-    {
-        if (Input.GetKeyDown("tab"))
-        {
-            panelSave.SetActive(!panelSave.activeSelf);
-            if (panelSave.activeSelf)
-            {
-                Time.timeScale = 0;
-                Cursor.visible = true;
-                this.GetComponent<vThirdPersonInput>().enabled = false;
-            }
-            else
-            {
-                Time.timeScale = 1;
-                this.GetComponent<vThirdPersonInput>().enabled = true;
-                //Cursor.visible = false;
-            }
-        }
-    }
+    //void LoadSystem()
+    //{
+    //    if (Input.GetKeyDown("tab"))
+    //    {
+    //        panelSave.SetActive(!panelSave.activeSelf);
+    //        if (panelSave.activeSelf)
+    //        {
+    //            Time.timeScale = 0;
+    //            Cursor.visible = true;
+    //            this.GetComponent<vThirdPersonInput>().enabled = false;
+    //        }
+    //        else
+    //        {
+    //            Time.timeScale = 1;
+    //            this.GetComponent<vThirdPersonInput>().enabled = true;
+    //            //Cursor.visible = false;
+    //        }
+    //    }
+    //}
 }
