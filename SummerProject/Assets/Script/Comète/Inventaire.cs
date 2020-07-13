ea79ac2f97bdc50f667f029660e7ca2a64fa9b59
Comete99 <62999111+Comete99@users.noreply.github.com>
2020-07-13 18:13:00 +0200

    Debut inventaire

---
+++ SummerProject/Assets/Script/Comète/Inventaire.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventaire : MonoBehaviour
+{
+    public GameObject inventory;
+    public int nbPlace = 154;
+    private int nbPlacesOccupees = 0;
+    public GameObject[] slots;
+    public List<GameObject> objects = new List<GameObject> { };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void addObject(int tailleObjet, GameObject objet)
+    {
+        if (nbPlacesOccupees + tailleObjet < nbPlace)
+        {
+            objet.transform.position = inventory.transform.position;
+            objects.Add(objet);
+            nbPlacesOccupees += tailleObjet;
+            //Instantiate(objet, inventory.transform, true);
+        }
+        else
+        {
+            Debug.Log("Inventaire plein");
+        }
+    }
+}
---
+++ SummerProject/Assets/Script/Comète/PickUp.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    private Inventaire inventaire;
+    public int tailleEmplacement = 1;
+
+    private void Start()
+    {
+        inventaire = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventaire>();
+    }
+
+    private void Update()
+    {
+        //if (Input.GetMouseButtonDown(1))
+        //{
+        //    RaycastHit hit;
+        //    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        //    if(Physics.Raycast(ray, out hit, 100.0f))
+        //    {
+        //        inventaire.addObject(tailleEmplacement, transform.gameObject);
+        //    }
+        //}
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            inventaire.addObject(tailleEmplacement, gameObject);
+            //Destroy(gameObject);
+        }
+    }
+}
